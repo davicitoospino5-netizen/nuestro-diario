d612967768c3081ea9b26b9a23925cd6b8b59b3b
Slum <davicitoospino5@gmail.com>
2026-07-05 18:42:01 -0500

    arreglos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'diario-cache-auto-v3';
+const CACHE_NAME = 'diario-cache-auto-v4';
 const assets = ['/', '/index.html', '/manifest.json', '/poemas.txt'];
 
 // Instalar el Service Worker y guardar los archivos básicos
